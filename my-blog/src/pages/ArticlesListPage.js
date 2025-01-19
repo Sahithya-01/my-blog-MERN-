943d4629dcb2927fa9fa1c9c7feaf837c9f034f6
“Sahithya <sahithya.hanumandla01@gmail.com>
2025-01-19 16:48:28 -0500

    my-blog app

---
+++ my-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,12 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
+const ArticlesListPage = () => {
+  return (
+    <>
+      <h1>Articles</h1>
+      <ArticlesList articles={articles} />
+    </>
+  );
+};
+export default ArticlesListPage;
